1324e92777fd13fbd840698696d4c18aa0eedd27
Muhammad Saeed <muhammad.saeed@student.rocva.nl>
2024-12-05 11:35:36 +0100

    commit van bestanden geupdate

---
+++ project.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const homeSection = document.getElementById("home");
+    const infoSection = document.getElementById("information");
+    const homeLink = document.querySelector('a[href="#home"]');
+    const infoLink = document.querySelector('a[href="informatie.html"]');
+
+    homeSection.classList.add("active");
+    infoSection.classList.add("inactive");
+
+
+    function showSection(sectionToShow, sectionToHide) {
+
+        sectionToShow.classList.remove("inactive");
+        sectionToShow.classList.add("active");
+
+        sectionToHide.classList.add("inactive");
+        sectionToHide.classList.remove("active");
+    }
+
+
+    homeLink.addEventListener("click", function() {
+        showSection(homeSection, infoSection);
+    });
+
+ 
+    infoLink.addEventListener("click", function() {
+        showSection(infoSection, homeSection);
+    });
+});
